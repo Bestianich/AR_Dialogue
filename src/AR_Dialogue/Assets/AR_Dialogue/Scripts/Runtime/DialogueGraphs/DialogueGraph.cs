3d998cf821aaba28e026cf3bb2477608d73cb2d0
Davide Fedeli <davidefedeli8899@gmail.com>
2026-05-22 15:28:20 +0200

    feat: change StartNode when adding a new StartNode in the DialogueGraph

--- src/AR_Dialogue/Assets/AR_Dialogue/Scripts/Runtime/DialogueGraphs/DialogueGraph.cs
+++ src/AR_Dialogue/Assets/AR_Dialogue/Scripts/Runtime/DialogueGraphs/DialogueGraph.cs
@@ -1,10 +1,24 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using XNode;
 
 [CreateAssetMenu(fileName = "newDialogueGraph", menuName = "AR_Dialogue/New Dialogue Graph")]
-public class ADialogueGraph : NodeGraph 
-{ 
+public class DialogueGraph : NodeGraph
+{
+    public StartNode StartNode;
+
+
+    //Check to set the StartNode of the DialogueGraph
+    public override Node AddNode(Type type)
+    {
+        if (type == typeof(StartNode))
+        {
+            StartNode = base.AddNode(type) as StartNode;
+            return StartNode;
+        }
+        return base.AddNode(type);
+    }
     
 }

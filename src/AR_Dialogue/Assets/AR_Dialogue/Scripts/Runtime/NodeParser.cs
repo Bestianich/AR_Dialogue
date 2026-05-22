ffded3946054d0af3fee558ce5669118bd52a348
Davide Fedeli <davidefedeli8899@gmail.com>
2026-05-22 15:28:55 +0200

    feat: NextNode logic in NodeParser

--- src/AR_Dialogue/Assets/AR_Dialogue/Scripts/Runtime/NodeParser.cs
+++ src/AR_Dialogue/Assets/AR_Dialogue/Scripts/Runtime/NodeParser.cs
@@ -3,9 +3,30 @@ using XNode;
 
 public class NodeParser : MonoBehaviour
 {
-        [SerializeField] private NodeGraph _graph;
+        [SerializeField] private DialogueGraph _graph;
 
+        private ANode _currentNode;
+        
         public void Parse()
+        { 
+            if (_currentNode == null) 
+                _currentNode = _graph.StartNode;
+            
+            NextNode("OutPort");
+        }
+
+        public void NextNode(string outPortField)
         {
+            //Check if exist OutPort
+            if(!_currentNode.HasPort(outPortField))
+            {
+                Debug.LogError(outPortField + " is not a valid port");
+                return;
+            }
+            
+            var port =  _currentNode.GetPort(outPortField);
+            var nextNode = port.Connection.node as ANode;
+            _currentNode = nextNode;
+            
         }
 }
